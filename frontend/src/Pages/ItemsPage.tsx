0971d237025f96f698e7811b99922f6fcab2ff71
simekva <han.simen.kvalo@gmail.com>
2025-07-10 12:33:22 +0200

    s

--- frontend/src/Pages/ItemsPage.tsx
+++ frontend/src/Pages/ItemsPage.tsx
@@ -3,6 +3,7 @@ import { ItemSlots } from "../Enums";
 import type { Item } from "../Interfaces";
 import { graphQlCLClient } from "../App";
 import { ITEMS_BY_ITEMSLOT_QUERY } from "../Queries/Queries";
+import { Header } from "../Components/Header";
 
 interface ItemsBySlotResponse {
   itemBySlot: Item[];
@@ -27,40 +28,63 @@ export function ItemsPage() {
   }
   return (
     <>
-      {/* Select item slot */}
-      <div>
-        <h3>Select item slot</h3>
-        <select onChange={(e) => setItemSlot(e.target.value as ItemSlots)}>
-          {Object.keys(ItemSlots).map((itemSlot) => (
-            <option key={itemSlot} value={itemSlot}>
-              {itemSlot}
-            </option>
-          ))}
-        </select>
-        {itemSlot}
-      </div>
+      <Header />
+      <div className="flex mx-auto max-w-3xl p-6 shadow-md rounded-md space-y-8 flex-col">
+        {/* Select item slot */}
+        <div>
+          <h2 className="text-xl font-semibold mb-4">Select item slot</h2>
+          <select
+            className="border rounded"
+            onChange={(e) => setItemSlot(e.target.value as ItemSlots)}
+          >
+            {Object.keys(ItemSlots).map((itemSlot) => (
+              <option key={itemSlot} value={itemSlot}>
+                {itemSlot}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      {/* Search button */}
-      <div>
-        <button
-          className="bg-green-400 px-8 py-1 text-white rounded"
-          onClick={handleSearch}
-          type="button"
-        >
-          Search
-        </button>
-      </div>
+        {/* Search button */}
+        <div className="">
+          <button
+            className="bg-green-400 px-8 py-1 text-white rounded"
+            onClick={handleSearch}
+            type="button"
+          >
+            Search
+          </button>
+        </div>
 
-      {/* List of items found*/}
-      <div>
-        <h3>Items</h3>
-        <ul>
-          {Array.isArray(items) && items.length > 0 ? (
-            items.map((item) => <li key={item.id}>{item.name}</li>)
-          ) : (
-            <li>No items found</li>
-          )}
-        </ul>
+        {/* List of items found*/}
+        <div>
+          <h3 className="text-xl font-semibold mb-4">Items</h3>
+          <ul>
+            {Array.isArray(items) && items.length > 0 ? (
+              items.map((item) => (
+                <li key={item.id} className="my-4">
+                  <b>Name:</b> {item.name}. Ilvl: {item.itemLevel}. Slot:{" "}
+                  {item.itemSlot}.
+                  <p key={item.id}>
+                    {item.stats.map((stat) => (
+                      <>
+                        {stat.key}: {stat.value} {","}
+                      </>
+                    ))}
+                  </p>
+                  Gem slots:{" "}
+                  {item.gemSlots.map((gemSlot) => (
+                    <>
+                      {gemSlot.color} {", "}
+                    </>
+                  ))}
+                </li>
+              ))
+            ) : (
+              <li key={null}>No items found</li>
+            )}
+          </ul>
+        </div>
       </div>
     </>
   );
